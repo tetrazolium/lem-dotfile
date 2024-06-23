2b62aebd2d85cd1958762d03205e9c3fdac66fd0
tetrazolium <115483176+tetrazolium@users.noreply.github.com>
2024-06-22 21:26:29 -0500

    Made paredit mode only insert braces when in vim insert mode

--- init.lisp
+++ init.lisp
@@ -2,8 +2,22 @@
 (lem-vi-mode:vi-mode)
 (add-hook lem-lisp-mode:*lisp-repl-mode-hook* 'lem-vi-mode/commands:vi-insert)
 (defun lem-paredit-hook ()
-  (lem-paredit-mode:paredit-mode t))
+  (lem-paredit-mode:paredit-mode t)
+  (define-key lem-paredit-mode:*paredit-mode-keymap* "(" nil)
+  (define-key lem-paredit-mode:*paredit-mode-keymap* ")" nil)
+  (define-key lem-paredit-mode:*paredit-mode-keymap* "[" nil)
+  (define-key lem-paredit-mode:*paredit-mode-keymap* "]" nil)
+  (define-key lem-paredit-mode:*paredit-mode-keymap* "{" nil)
+  (define-key lem-paredit-mode:*paredit-mode-keymap* "}" nil)
+  (define-key lem-vi-mode:*insert-keymap* "(" 'paredit-insert-paren)
+  (define-key lem-vi-mode:*insert-keymap* ")" 'paredit-close-parenthesis)
+  (define-key lem-vi-mode:*insert-keymap* "[" 'paredit-insert-bracket)
+  (define-key lem-vi-mode:*insert-keymap* "]" 'paredit-close-bracket)
+  (define-key lem-vi-mode:*insert-keymap* "{" 'paredit-insert-brace)
+  (define-key lem-vi-mode:*insert-keymap* "}" 'paredit-close-bracke)
+  )
 (add-hook lem-lisp-mode:*lisp-mode-hook* #'lem-paredit-hook)
+;Unbreak "{" and "}" in lisp-mode
 
 
 (defun define-keys-in-maps (&rest keymap-pairs)
@@ -16,3 +30,6 @@
 
 (define-keys-in-maps *new-paredit-keys*)
 (ql:quickload "lem/legit")
+;Unbreak up and down in vi-mode
+;(define-key lem/peek-legit:*peek-legit-keymap* "d" 'lem/peek-legit:peek-legit-discard-file)
+;(define-key lem/peek-legit:*peek-legit-keymap* "k" nil)
